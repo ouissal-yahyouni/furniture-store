b25f865b3c1e2c90d6845a320396616628b294c9
ouissal-yahyouni <ouissalyahyouni73@gmail.com>
2025-09-29 19:48:20 +0100

    Initial commit - furniture store project

---
+++ furniture-store/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Furniture Store</h1>
+    </>
+  )
+}
+
+export default App
